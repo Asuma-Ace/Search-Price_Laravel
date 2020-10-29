440446de551f11aeb17eaa2c6a7e3fddcef04f8a
samua <asuma.business2@gmail.com>
2020-10-29 12:34:21 +0900

    modify script path

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -3,7 +3,7 @@
 <!-- jQuery -->
 <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
 <!-- Bootstrap tooltips -->
-<script src="{{ asset('join/js/popper.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
 <!-- Bootstrap core JavaScript -->
 <script src="{{ asset('js/app.js') }}"></script>
 <!-- MDB core JavaScript -->
