1fb7eac3af8e4afc9795d80fb3fc3d8da8179394
samua <asuma.business2@gmail.com>
2020-10-30 10:29:19 +0900

    modify logout function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        return redirect('/user/logout');
+    }
 }
