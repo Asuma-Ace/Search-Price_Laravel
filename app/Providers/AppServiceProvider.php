c2f72c927808ed1c798016c996ef963d098f9068
samua <asuma.business2@gmail.com>
2020-11-01 22:12:14 +0900

    add comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         Schema::defaultStringLength(191);
+        // ローカル環境で動かす場合以下3行をコメントアウトする
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
